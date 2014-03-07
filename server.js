fc67631029a49cddfea1cfe1b3bb3cb3e534fd99
Camille Bissuel <welcome@nylnook.com>
2014-03-07 10:57:47 +0100

    try deployement based on Express.js and vhost

--- server.js
+++ server.js
@@ -1,12 +1,46 @@
-var docpadInstanceConfiguration = {
-    env: 'production'
-};
+// Create Server and Express Application
+var express = require('express');
+var http = require('http');
+var app = express();
+var server = http.createServer(app).listen(8080);
 
-require('docpad').createInstance(docpadInstanceConfiguration, function(err,docpadInstance){
+// Add our Application Middlewares
+app.use(app.router);
+
+// Add DocPad to our Application
+var docpadInstanceConfiguration = {
+    // Give it our express application and http server
+    serverExpress: app,
+    serverHttp: server,
+
+    // Tell it not to load the standard middlewares (as we handled that above)
+    middlewareStandard: false
+};
+var docpadInstance = require('docpad').createInstance(docpadInstanceConfiguration, function(err){
     if (err)  return console.log(err.stack);
-    var generateOpts = {};
-    docpadInstance.action('generate server watch', generateOpts, function(err,result){
+
+    // Tell DocPad to perform a generation, extend our server with its routes, and watch for changes
+    docpadInstance.action('generate server watch', function(err){
         if (err)  return console.log(err.stack);
-        // ...
     });
 });
+
+// Continue with your application
+// ...
+
+// Redirect app
+
+var redirect = express();
+
+redirect.all('*', function(req, res){
+  console.log(req.subdomains);
+  res.redirect('http://nylnook.com:8080/' + req.subdomains[0]);
+});
+
+// Vhost app
+
+app.use(vhost('*.nylnook.com', redirect)) // Serves all subdomains via Redirect app
+app.use(vhost('nylnook.com', main)); // Serves top level domain via Main server app
+
+app.listen(8080);
+console.log('Express app started on port 8080');
