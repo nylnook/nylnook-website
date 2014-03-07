218f23f300538d26c9794176da94dd90b01d4199
Camille Bissuel <welcome@nylnook.com>
2014-03-07 10:15:39 +0100

    try deployement based on Docpad API

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-module.exports = require(__dirname+'/node_modules/docpad/out/bin/docpad-server');
+var docpadInstanceConfiguration = {
+    env: 'production'
+};
+
+require('docpad').createInstance(docpadInstanceConfiguration, function(err,docpadInstance){
+    if (err)  return console.log(err.stack);
+    var generateOpts = {};
+    docpadInstance.action('generate server watch', generateOpts, function(err,result){
+        if (err)  return console.log(err.stack);
+        // ...
+    });
+});
