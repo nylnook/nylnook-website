1c02bb38771d85676e00eb174d17f6fbfa73fef3
Camille Bissuel <welcome@nylnook.com>
2014-03-18 09:34:11 +0100

    try pflannery solution at https://github.com/bevry/docpad/issues/819

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-node node_modules/docpad/bin/docpad-server
+var spawn = require('child_process').spawn
+
+var args = ['server'];
+spawn('node_modules/.bin/docpad', args, {stdio:'inherit'}).on('close', function(err) {
+
+    if (err)
+        console.log("docpad failed");
+    else
+        console.log("docpad running");
+
+});
