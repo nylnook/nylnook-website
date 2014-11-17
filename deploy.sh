7caa5237a3e80b739bfe313fb1186f70700279fe
Camille Bissuel <welcome@nylnook.com>
2014-11-17 14:50:54 +0100

    additional static fixes

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-$DATE = date
+DATE=`date`
 docpad clean
 docpad generate --env static
 cp -ur out/* ../nylnook.github.io/
 cd ../nylnook.github.io/
 git add --all
-git commit -m '$DATE'
-git push master origin
+git commit -m "$DATE"
+git push
 cd ../nylnook-website/
