58b3bce6904593bb42d54ff5fc5cb6469528f1c6
Camille Bissuel <welcome@nylnook.com>
2014-11-17 14:12:14 +0100

    not using ghpages plugin

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+$DATE = date
+docpad clean
+docpad generate --env static
+cp -ur out/* ../nylnook.github.io/
+cd ../nylnook.github.io/
+git add --all
+git commit -m '$DATE'
+git push master origin
+cd ../nylnook-website/
